2d649a1b7bc46ca709020830174678b9b896cdfc
Sidox <pro.sidox@gmail.com>
2023-03-11 22:42:30 +0100

    html in body mail

--- pages/api/send/[userEmail]/[subject]/[message].ts
+++ pages/api/send/[userEmail]/[subject]/[message].ts
@@ -5,7 +5,24 @@ import secrets from '../../../../../secrets.json';
 export default function handler(req: any, res: any) {
     const { userEmail, subject, message } = req.query;
 
-    // res.status(200).json({ userEmail, subject, message });
+    let content = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Invitation à AKHA</title>
+        <style>
+          /* Styles CSS */
+        </style>
+      </head>
+      <body>
+      <img class="logo" src="https://firebasestorage.googleapis.com/v0/b/trafik-33b5a.appspot.com/o/120.png?alt=media&token=6f5f5a89-ae51-4dee-bf23-4090c89b43c1" alt="Logo AKHA">
+        <h1>Bienvenue à AKHA!</h1>
+        <p>Vous avez été invité à rejoindre notre application.</p>
+        <p>Cliquez sur le lien suivant pour vous inscrire: <a href="https://www.akha.com/register">https://www.akha.com/register</a></p>
+      </body>
+    </html>
+`;
+
 
     let transporter = nodemailer.createTransport({
         service: "gmail",
@@ -18,8 +35,8 @@ export default function handler(req: any, res: any) {
     let mailOptions = {
         from: "no.reply.akha@gmail.com",
         to: userEmail,
-        subject: message,
-        text: subject,
+        subject: subject,
+        html: content,
     };
 
     transporter.sendMail(mailOptions, function (error: any, info: any) {
